9b45a742239d60e39315e88a9093f84e18f09c88
Domanova.Maria <domanova@mail.ru>
2024-05-12 19:31:03 +0500

    Init commit

---
+++ app_blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
+from rest_framework import viewsets
+from .serializers import PostSerializer
+from .models import Post
+
+# Create your views here.
+class PostList(generic.ListView): 
+    queryset = Post.objects.filter(status=1).order_by('-created_on') 
+    template_name = 'index.html' 
+
+class PostDetail(generic.DetailView): 
+    model = Post 
+    template_name = 'post_detail.html' 
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
