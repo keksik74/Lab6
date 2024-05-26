7f70c6cce091e1e512e2c392d94c6c62d51fcda3
Domanova.Maria <domanova@mail.ru>
2024-05-26 23:38:14 +0500

    Init commit

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -7,7 +7,10 @@ router.register(r'posts', views.PostViewSet, basename='post')
  
 
 urlpatterns = [ 
-    #path('', views.PostList.as_view(), name='home'), 
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'), 
+    path('', views.PostList.as_view(), name='home'), 
+    path('blog/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'), 
+    path('register/',views.register_request, name='register'),
+    path('login/',views.login_request,name='login'),
+    path('logout/',views.logout_request,name='logout'),
     path('api/v1/',include(router.urls))
 ] 
--- app_blog/views.py
+++ app_blog/views.py
@@ -4,7 +4,11 @@ from .models import Post
 from rest_framework import viewsets
 from .serializers import PostSerializer
 from .models import Post
-
+from .forms import NewUserForm
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm 
 # Create your views here.
 class PostList(generic.ListView): 
     queryset = Post.objects.filter(status=1).order_by('-created_on') 
@@ -18,3 +22,40 @@ class PostDetail(generic.DetailView):
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+
+
+def register_request(request): 
+  if request.method == "POST": 
+    form = NewUserForm(request.POST) 
+    if form.is_valid(): 
+      user = form.save() 
+      login(request, user) 
+      messages.success(request, "Registration successful." ) 
+      return redirect("home") 
+    messages.error(request, "Unsuccessful registration. Invalid information.") 
+  form = NewUserForm() 
+  return render (request=request, template_name="register.html", context={"register_form":form}) 
+
+
+def login_request(request): 
+  if request.method == "POST": 
+    form = AuthenticationForm(request, data=request.POST) 
+    if form.is_valid(): 
+      username = form.cleaned_data.get('username') 
+      password = form.cleaned_data.get('password') 
+      user = authenticate(username=username, password=password) 
+      if user is not None: 
+        login(request, user) 
+        messages.info(request, f"You are now logged in as {username}.") 
+        return redirect("home") 
+      else: 
+        messages.error(request,"Invalid username or password.") 
+    else: 
+      messages.error(request,"Invalid username or password.") 
+  form = AuthenticationForm() 
+  return render(request=request, template_name="login.html", context={"login_form":form}) 
+
+def logout_request(request): 
+    logout(request) 
+    messages.info(request, "You have successfully logged out.")  
+    return redirect("home") 
